e52564db0497a3cd0a3ced6f3925178fcb38dcc3
app318 <app318@gmail.com>
2020-07-14 12:21:25 +0800

    First commit

---
+++ source/categories/index.md
@@ -0,0 +1,5 @@
+---
+title: 分类
+date: 2020-03-26 21:02:08
+type: "categories"
+---
---
+++ themes/next/source/dist/music.js
@@ -0,0 +1,43 @@
+﻿const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: '可爱即正义',
+        artist: 'BobbyER,夜の律',
+        url: 'https://pan.f2008.cn/%E5%8F%AF%E7%88%B1%E5%8D%B3%E6%AD%A3%E4%B9%89.mp3',
+        cover: 'https://p2.music.126.net/_ysOEskrYvMRWWK6BNrPjw==/109951163896982374.jpg?param=130y130',
+      },
+      {
+        name: '魔力夏日',
+        artist: '麦小兜',
+        url: 'https://pan.f2008.cn/%E9%AD%94%E5%8A%9B%E5%A4%8F%E6%97%A5.mp3',
+        cover: 'https://p2.music.126.net/otip9SS8jspSSKN_b2vnLQ==/109951163469410515.jpg?param=130y130',
+      },
+      {
+        name: 'Only The Young (Featured in Miss Americana)',
+        artist: 'Taylor Swift',
+        url: 'https://pan.f2008.cn/Only%20The%20Young%20(Featured%20in%20Miss%20Americana).mp3',
+        cover: 'https://p2.music.126.net/5tD7nHxIVs6akS8PUXcCrw==/109951164663333512.jpg?param=130y130',
+      },
+      {
+        name: '梦童话(Instrument)',
+        artist: '灰澈',
+        url: 'https://pan.f2008.cn/%E6%A2%A6%E7%AB%A5%E8%AF%9D(Instrument).mp3',
+        cover: 'https://p2.music.126.net/y0n35bYIISSJrvsg73or1A==/109951163013138274.jpg?param=130y130',
+      },
+      {
+        name: '流浪地图',
+        artist: '孙燕姿',
+        url: 'https://pan.f2008.cn/%E6%B5%81%E6%B5%AA%E5%9C%B0%E5%9B%BE%20-%20%E5%AD%99%E7%87%95%E5%A7%BF.mp3',
+        cover: 'https://p1.music.126.net/MKIERQMSw1ohk7JY9waXHA==/109951163250087443.jpg?param=130y130',
+      },
+      {
+        name: '金曜日の国語课',
+        artist: 'SimorE',
+        url: 'https://pan.f2008.cn/%E9%87%91%E6%9B%9C%E6%97%A5%E3%81%AE%E5%9B%BD%E8%AA%9E%E8%AF%BE.mp3',
+        cover: 'https://p1.music.126.net/TdcgUDP_6VeqfBAVawmIzQ==/109951162885748687.jpg?param=130y130',
+      }
+    ]
+});
